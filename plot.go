3046df54fa902eb77774354ac451820cc1cc6a46
dbaseqp <gabrielfok128@gmail.com>
2024-04-04 07:54:17 +0000

    team pcr & typo & graph

--- plot.go
+++ plot.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
-	"math"
 	"os"
 	"path/filepath"
-	"time"
+	"sort"
+	"strconv"
 
-	"git.sr.ht/~sbinet/gg"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font/gofont/goregular"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 	"gonum.org/v1/plot/vg/draw"
 	"gonum.org/v1/plot/vg/vgimg"
@@ -43,138 +40,221 @@ func makeGraphs() error {
 		return err
 	}
 
-	dataThisRound, err := dbGetChecksThisRound(roundNumber - 1)
+	// dataThisRound, err := dbGetChecksThisRound(roundNumber - 1)
+	// if err != nil {
+	// 	return err
+	// }
+
+	dataScores, err := dbGetTeams()
 	if err != nil {
 		return err
 	}
+	sort.Slice(dataScores, func(i, j int) bool {
+		return dataScores[i].CumulativeServiceScore < dataScores[j].CumulativeServiceScore
+	})
 
 	graphScoresOverTime(dataOverTime, "plots/points-over-time-light.png", lightmode)
 	graphScoresOverTime(dataOverTime, "plots/points-over-time-dark.png", darkmode)
-	graphCurrentChecks(dataThisRound, "plots/current-status-light.png", lightmode)
-	graphCurrentChecks(dataThisRound, "plots/current-status-dark.png", darkmode)
+	graphTotalScores(dataScores, "plots/scores-light.png", lightmode)
+	graphTotalScores(dataScores, "plots/scores-dark.png", darkmode)
+	// graphCurrentChecks(dataThisRound, "plots/current-status-light.png", lightmode)
+	// graphCurrentChecks(dataThisRound, "plots/current-status-dark.png", darkmode)
 
 	return nil
 }
 
-func graphCurrentChecks(data map[uint][]CheckData, path string, config GraphConfig) error {
-	W := 945
-	H := 454
-	// const W = 50
-	// const H = 50
+func graphTotalScores(data []TeamData, path string, config GraphConfig) error {
+	plot := plot.New()
 
-	if len(data) == 0 {
-		return nil
+	var scores []float64
+	var names []string
+	var placements []string
+	for i, team := range data {
+		scores = append(scores, float64(team.CumulativeServiceScore))
+		names = append(names, team.Name)
+		placements = append(placements, strconv.Itoa(len(data)-i))
 	}
 
-	upIcon, err := gg.LoadPNG("assets/services/up.png")
-	if err != nil {
-		errorPrint(err)
-	}
-	downIcon, err := gg.LoadPNG("assets/services/down.png")
-	if err != nil {
-		errorPrint(err)
-	}
-	uiw, uih := upIcon.Bounds().Dx(), upIcon.Bounds().Dy()
-	// diw, dih := upIcon.Bounds().Dx(), downIcon.Bounds().Dy()
-	teams, err := dbGetTeams()
+	bars, err := plotter.NewBarChart(plotter.Values(scores), vg.Points(50))
 	if err != nil {
 		return err
 	}
 
-	xoffset := int(iconWidth * 1.3)
-	yoffset := int(iconHeight * 1.3)
-	scaleX := iconWidth / float64(uiw)
-	scaleY := iconHeight / float64(uih)
+	bars.Horizontal = true
 
-	dc := gg.NewContext(W, H)
-	font, err := truetype.Parse(goregular.TTF)
+	bars.LineStyle.Width = vg.Length(0)
+	bars.Color = plotutil.Color(1)
+	plot.X.Label.TextStyle.Color = config.Color
+	plot.X.Color = config.Color
+	plot.X.Tick.Color = config.Color
+	plot.X.Tick.Label.Color = config.Color
+	plot.Y.Label.TextStyle.Color = config.Color
+	plot.Y.Color = config.Color
+	plot.Y.Tick.Color = config.Color
+	plot.Y.Tick.Label.Color = config.Color
+	plot.Legend.TextStyle.Color = config.Color
+	plot.BackgroundColor = color.Transparent
+	plot.NominalY(placements...)
+
+	plot.Add(bars)
+	var xys []plotter.XY
+	for i := range scores {
+		xys = append(xys, plotter.XY{X: scores[i] + 1, Y: float64(i)})
+	}
+	labels, err := plotter.NewLabels(plotter.XYLabels{
+		XYs:    xys,
+		Labels: names,
+	},
+	)
 	if err != nil {
-		panic("")
+		return err
 	}
-	face := truetype.NewFace(font, &truetype.Options{
-		Size: fontSize,
-	})
-	dc.SetFontFace(face)
-
-	var maxTeamNameLength float64
-	maxTeamNameLength = 0
-	for _, team := range teams {
-		if w, _ := dc.MeasureString(team.Name); w > maxTeamNameLength {
-			maxTeamNameLength = w
-		}
+	for i := range labels.TextStyle {
+		labels.TextStyle[i].Color = config.Color
 	}
+	plot.Add(labels)
 
-	// get keys and sort them so that rendering is deterministic
-	dc.Push()
-	dc.Rotate(gg.Degrees(30))
-	var maxServiceNameHeight float64
-	maxServiceNameHeight = 0
-	var services []string
-	for _, check := range data[teams[0].ID] {
-		services = append(services, check.ServiceName)
-		if w, h := dc.MeasureString(check.ServiceName); (w*math.Sin(math.Pi/float64(6)))+(h*math.Cos(math.Pi/float64(6))) > maxServiceNameHeight {
-			maxServiceNameHeight = (w * math.Sin(math.Pi/float64(6))) + (h * math.Cos(math.Pi/float64(6)))
-		}
+	canvas := vgimg.PngCanvas{Canvas: vgimg.NewWith(
+		vgimg.UseWH(25*vg.Centimeter, 12*vg.Centimeter),
+		vgimg.UseBackgroundColor(color.Transparent),
+	)}
+	plot.Draw(draw.New(canvas))
+
+	// Save the plot to a png file
+	f, err := os.Create(path)
+	if err != nil {
+		errorPrint(err)
+		return err
 	}
+	defer f.Close()
 
-	if int(maxServiceNameHeight)+(len(teams)*yoffset) > H {
-		H = int(maxServiceNameHeight) + (len(teams) * yoffset) + fontSize + (yoffset * 2)
+	_, err = canvas.WriteTo(f)
+	if err != nil {
+		errorPrint(err)
+		return err
 	}
-	if int(maxTeamNameLength)+(len(services)*xoffset) > W {
-		W = int(maxTeamNameLength) + (len(services) * xoffset)
-	}
-	dc = gg.NewContext(W, H)
-
-	dc.SetFontFace(face)
-	dc.SetColor(config.Color)
-	dc.Translate(maxTeamNameLength, maxServiceNameHeight)
-
-	dc.Push()
-	dc.Translate(float64(xoffset), 0)
-	face = truetype.NewFace(font, &truetype.Options{
-		Size: fontSize * 2,
-	})
-	dc.SetFontFace(face)
-	dc.DrawStringAnchored(fmt.Sprint("Service Status: ", time.Now().In(loc).Format("2006-01-02 15:04:05")), 0, 0, 0, 0)
-	dc.Pop()
-	dc.Translate(0, float64(yoffset*2))
-
-	dc.Push()
-	dc.Translate(float64(xoffset/2), 0)
-	for _, serviceName := range services {
-		dc.Push()
-		dc.Rotate(gg.Radians(30))
-		dc.DrawStringAnchored(serviceName, 0, 0, 1, 0.5)
-		dc.Pop()
-		dc.Translate(float64(xoffset), 0)
-	}
-	dc.Pop()
-	dc.Translate(0, fontSize/2)
-
-	for _, team := range teams {
-		dc.Push()
-		dc.DrawStringAnchored(team.Name, 0, float64(yoffset/2), 1, 0.5)
-		for _, check := range data[team.ID] {
-			dc.Push()
-			dc.DrawRectangle(0, 0, float64(xoffset), float64(yoffset))
-			dc.Stroke()
-			dc.Translate(float64(xoffset/2), 0)
-			dc.Scale(scaleX, scaleY)
-			if check.Result == true {
-				dc.DrawImageAnchored(upIcon, 0, (yoffset / 2), 0.5, 0)
-			} else {
-				dc.DrawImageAnchored(downIcon, 0, (yoffset / 2), 0.5, 0)
-			}
-			dc.Pop()
-			dc.Translate(float64(xoffset), 0)
-		}
-		dc.Pop()
-		dc.Translate(0, float64(yoffset))
-	}
-
-	dc.SavePNG(path)
 	return nil
 }
+
+// func graphCurrentChecks(data map[uint][]CheckData, path string, config GraphConfig) error {
+// 	W := 945
+// 	H := 454
+// 	// const W = 50
+// 	// const H = 50
+
+// 	if len(data) == 0 {
+// 		return nil
+// 	}
+
+// 	upIcon, err := gg.LoadPNG("assets/services/up.png")
+// 	if err != nil {
+// 		errorPrint(err)
+// 	}
+// 	downIcon, err := gg.LoadPNG("assets/services/down.png")
+// 	if err != nil {
+// 		errorPrint(err)
+// 	}
+// 	uiw, uih := upIcon.Bounds().Dx(), upIcon.Bounds().Dy()
+// 	// diw, dih := upIcon.Bounds().Dx(), downIcon.Bounds().Dy()
+// 	teams, err := dbGetTeams()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	xoffset := int(iconWidth * 1.3)
+// 	yoffset := int(iconHeight * 1.3)
+// 	scaleX := iconWidth / float64(uiw)
+// 	scaleY := iconHeight / float64(uih)
+
+// 	dc := gg.NewContext(W, H)
+// 	font, err := truetype.Parse(goregular.TTF)
+// 	if err != nil {
+// 		panic("")
+// 	}
+// 	face := truetype.NewFace(font, &truetype.Options{
+// 		Size: fontSize,
+// 	})
+// 	dc.SetFontFace(face)
+
+// 	var maxTeamNameLength float64
+// 	maxTeamNameLength = 0
+// 	for _, team := range teams {
+// 		if w, _ := dc.MeasureString(team.Name); w > maxTeamNameLength {
+// 			maxTeamNameLength = w
+// 		}
+// 	}
+
+// 	// get keys and sort them so that rendering is deterministic
+// 	dc.Push()
+// 	dc.Rotate(gg.Degrees(30))
+// 	var maxServiceNameHeight float64
+// 	maxServiceNameHeight = 0
+// 	var services []string
+// 	for _, check := range data[teams[0].ID] {
+// 		services = append(services, check.ServiceName)
+// 		if w, h := dc.MeasureString(check.ServiceName); (w*math.Sin(math.Pi/float64(6)))+(h*math.Cos(math.Pi/float64(6))) > maxServiceNameHeight {
+// 			maxServiceNameHeight = (w * math.Sin(math.Pi/float64(6))) + (h * math.Cos(math.Pi/float64(6)))
+// 		}
+// 	}
+
+// 	if int(maxServiceNameHeight)+(len(teams)*yoffset) > H {
+// 		H = int(maxServiceNameHeight) + (len(teams) * yoffset) + fontSize + (yoffset * 2)
+// 	}
+// 	if int(maxTeamNameLength)+(len(services)*xoffset) > W {
+// 		W = int(maxTeamNameLength) + (len(services) * xoffset)
+// 	}
+// 	dc = gg.NewContext(W, H)
+
+// 	dc.SetFontFace(face)
+// 	dc.SetColor(config.Color)
+// 	dc.Translate(maxTeamNameLength, maxServiceNameHeight)
+
+// 	dc.Push()
+// 	dc.Translate(float64(xoffset), 0)
+// 	face = truetype.NewFace(font, &truetype.Options{
+// 		Size: fontSize * 2,
+// 	})
+// 	dc.SetFontFace(face)
+// 	dc.DrawStringAnchored(fmt.Sprint("Service Status: ", time.Now().In(loc).Format("2006-01-02 15:04:05")), 0, 0, 0, 0)
+// 	dc.Pop()
+// 	dc.Translate(0, float64(yoffset*2))
+
+// 	dc.Push()
+// 	dc.Translate(float64(xoffset/2), 0)
+// 	for _, serviceName := range services {
+// 		dc.Push()
+// 		dc.Rotate(gg.Radians(30))
+// 		dc.DrawStringAnchored(serviceName, 0, 0, 1, 0.5)
+// 		dc.Pop()
+// 		dc.Translate(float64(xoffset), 0)
+// 	}
+// 	dc.Pop()
+// 	dc.Translate(0, fontSize/2)
+
+// 	for _, team := range teams {
+// 		dc.Push()
+// 		dc.DrawStringAnchored(team.Name, 0, float64(yoffset/2), 1, 0.5)
+// 		for _, check := range data[team.ID] {
+// 			dc.Push()
+// 			dc.DrawRectangle(0, 0, float64(xoffset), float64(yoffset))
+// 			dc.Stroke()
+// 			dc.Translate(float64(xoffset/2), 0)
+// 			dc.Scale(scaleX, scaleY)
+// 			if check.Result == true {
+// 				dc.DrawImageAnchored(upIcon, 0, (yoffset / 2), 0.5, 0)
+// 			} else {
+// 				dc.DrawImageAnchored(downIcon, 0, (yoffset / 2), 0.5, 0)
+// 			}
+// 			dc.Pop()
+// 			dc.Translate(float64(xoffset), 0)
+// 		}
+// 		dc.Pop()
+// 		dc.Translate(0, float64(yoffset))
+// 	}
+
+// 	dc.SavePNG(path)
+// 	return nil
+// }
 
 func graphScoresOverTime(data map[uint][]RoundPointsData, path string, config GraphConfig) error {
 	plot := plot.New()
@@ -192,7 +272,6 @@ func graphScoresOverTime(data map[uint][]RoundPointsData, path string, config Gr
 	plot.Legend.TextStyle.Color = config.Color
 	plot.Y.Label.Text = "Score"
 	plot.Y.Width = 2
-	plot.BackgroundColor = color.Transparent
 	plot.BackgroundColor = color.Transparent
 
 	// graphData := make([]interface{}, len(data)*2)
