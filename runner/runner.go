0b61a12ac355323cb124fd6891361cf79420306d
dbaseqp <gabrielfok128@gmail.com>
2025-03-25 00:17:09 -0700

    fix event handling

--- runner/runner.go
+++ runner/runner.go
@@ -29,25 +29,18 @@ func main() {
 	log.Printf("[Runner] Started, listening for tasks on Redis at: %s", redisAddr)
 
 	go func() {
-		for {
-			val, err := rdb.BLPop(ctx, 0, "events").Result()
-			if err != nil {
-				log.Printf("[Runner] Error getting event: %v", err)
+		events := rdb.Subscribe(context.Background(), "events")
+		defer events.Close()
+		eventsChannel := events.Channel()
+
+		for msg := range eventsChannel {
+			log.Printf("[Runner] Received message: %v", msg)
+			if msg.Payload == "reset" {
+				log.Printf("[Runner] Reset event received, quitting...")
+				os.Exit(0)
+			} else {
 				continue
 			}
-
-			if len(val) < 2 {
-				log.Printf("[Runner] Invalid BLPop response: %v", val)
-				continue
-			}
-
-			if val[1] != "reset" {
-				log.Printf("[Runner] Invalid event payload: %v", val[1])
-				continue
-			}
-
-			log.Printf("[Runner] Reset event received, quitting...")
-			os.Exit(0)
 		}
 	}()
 
