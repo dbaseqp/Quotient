89811d1f2415291f51ea31bebdfe7b502d80a321
Tirefire <84106878+tire-fire@users.noreply.github.com>
2025-11-08 17:20:30 -0600

    Add CompetitionStart config and metadata endpoint
    Fixes #38, #77

--- www/api/api.go
+++ www/api/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+	"net/http"
 	"quotient/engine"
 	"quotient/engine/config"
 	"strings"
@@ -23,4 +25,26 @@ func SetEngine(e *engine.ScoringEngine) {
 // It ensures that the path is within the base directory
 func PathIsInDir(base string, relative string) bool {
 	return strings.HasPrefix(relative, base)
+}
+
+// CheckCompetitionStarted returns false and writes error response if competition hasn't started
+// Admins always have access regardless of competition start time
+func CheckCompetitionStarted(w http.ResponseWriter, r *http.Request) bool {
+	roles := r.Context().Value("roles")
+	if roles != nil {
+		roleList := roles.([]string)
+		for _, role := range roleList {
+			if role == "admin" {
+				return true
+			}
+		}
+	}
+
+	if !conf.HasCompetitionStarted() {
+		w.WriteHeader(http.StatusForbidden)
+		d, _ := json.Marshal(map[string]string{"error": "Competition has not started"})
+		w.Write(d)
+		return false
+	}
+	return true
 }
